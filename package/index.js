b3988c530108c494a27fc0d744a909b17a558c4c
STERIN SAJI <39603900+STERIN557@users.noreply.github.com>
2022-10-29 00:28:26 +0530

    some changes

---
+++ package/index.js
@@ -0,0 +1,15 @@
+import {
+  magnifyInitialize,
+  removeBoundary,
+  setMangifyProperties,
+} from './dist/main.js';
+
+function MangnifyIt() {
+  return {
+    magnifyInitialize,
+    removeBoundary,
+    setMangifyProperties,
+  };
+}
+
+module.exports = MangnifyIt;
--- test-folder/script.js
+++ test-folder/script.js
@@ -1,2 +1,21 @@
+// import {
+//     magnifyInitialize,
+//     removeBoundary,
+//     setMangifyProperties,
+//   } from './dist/main.js';
+
+//   magnifyInitialize({
+//     imageClassName: 'asali-image',
+//     lensClassName: 'lenskaclass',
+//     imagePreviewClassName: 'preview-dekho',
+//   });
+
+//   setMangifyProperties({
+//     lensBorderColor: 'brown',
+//     lensBorderSize: 5,
+//     lensColor: 'rgba(255,255,255,0.1)',
+//   });
+//   // removeBoundary(true);
+
 const magnifyit = require('magnifyit');
 console.log(magnifyit);
